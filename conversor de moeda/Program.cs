5a1d0b247d0be191d50a91bb80eeb08943165c87
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-19 20:12:14 -0300

    Add project files.

---
+++ conversor de moeda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ConversorDeMoeda
+{
+    static void Main(string[] args)
+    {
+        double cotacao, dolares, reais;
+
+        Console.Write("Digite a cotação do dólar: ");
+        cotacao = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor em dólares: ");
+        dolares = double.Parse(Console.ReadLine());
+
+        reais = cotacao * dolares;
+
+        Console.WriteLine("O valor em reais é: " + reais.ToString("F2"));
+    }
+}
